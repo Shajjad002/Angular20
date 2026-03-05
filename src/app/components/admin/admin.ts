e0638b4ff06c210cd625c8722add7adcb7c53197
Shajjad002 <shajjd002@gmail.com>
2026-03-05 15:45:20 +0600

    user admin component

---
+++ src/app/components/admin/admin.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css',
+})
+export class Admin {
+
+  firstName = 'John';
+  coursName = 'Angular 20';
+  rollNo = 111;
+
+  
+}
