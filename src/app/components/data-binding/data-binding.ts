2d02bfe287b0e41b94733fc3f65efcc480a408e2
Shajjad002 <shajjd002@gmail.com>
2026-03-09 16:47:19 +0600

    data binding

--- src/app/components/data-binding/data-binding.ts
+++ src/app/components/data-binding/data-binding.ts
@@ -5,16 +5,25 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css',
+  
 })
 export class DataBinding {
   courseName: string = 'Angular 20 Tutorial';
   protectedPrice: number = 12500;
   isAvailable: boolean = true;
+  maxLength: number = 5;
+  minCharacters: number = 3;
+  inputType: string = 'radio';
+
+  myClassName: string = 'myColor';
 
   constructor() {
     setTimeout(() => {
       this.courseName = 'Updated Angular 20 Tutorial';
     }, 3000);
+  }
+  showAlert() {
+    alert('Welcome to Angular 20 Tutorial');
   }
 
 }
--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './user.html',
-  styleUrl: './user.css',
+  styleUrls: ['./user.css'],
 })
 export class User {
 
